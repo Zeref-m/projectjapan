25c8c573d37e2b8919e46ebfeb35a649773d596f
Zeref-m <avoronin0406@gmail.com>
2024-11-10 12:03:25 +0300

    change modal

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -3,10 +3,16 @@
 import "./Gallery.css";
 import TourBlock from "./tour-block";
 import { useInView } from "react-intersection-observer";
-
+import Modal from "./components/Modal";
 
 export default function () {
-    const { ref, inView, entry } = useInView({threshold: .5, triggerOnce: true});
+    const { ref, inView, entry } = useInView({
+        threshold: 0.5,
+        triggerOnce: true,
+    });
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     return (
         <section ref={ref} id="gallery" className="gallery">
@@ -15,25 +21,34 @@ export default function () {
                 {inView}
             </div>
 
-            <div className={inView ? "tour-blocks" : 'd-none'}>
-                <TourBlock className="tokio-block">
+            <div className={inView ? "tour-blocks" : "d-none"}>
+                <TourBlock className="tokio-block" onClick={openModal}>
                     <p className="block-text">TOUR 01</p>
                     <p className="block-text text-red">TOKIO</p>
                 </TourBlock>
 
-                <TourBlock className="kyoto-block">
+                <TourBlock className="kyoto-block" onClick={openModal}>
                     <p className="block-text">TOUR 02</p>
                     <p className="block-text text-red">KYOTO</p>
                 </TourBlock>
 
-                <TourBlock className="fuji-block">
+                <TourBlock className="fuji-block" onClick={openModal}>
                     <p className=" block-text">TOUR 03</p>
                     <p className="block-text text-red">MOUNT FUJIYAMA</p>
                 </TourBlock>
-                <TourBlock className="kamakura-block">
+                <TourBlock className="kamakura-block" onClick={openModal}>
                     <p className=" block-text">TOUR 04</p>
                     <p className="block-text text-red">KAMAKURA</p>
                 </TourBlock>
+                <Modal isOpen={isModalOpen} onClose={closeModal}>
+                    <div d-flex>
+                        <div className='product-card'></div>
+                        <h1></h1>
+                        <p></p>
+                        <button>book a tour</button>
+                    </div>
+                    
+                </Modal>
             </div>
             <div className="d-flex more-tours">
                 <div>
